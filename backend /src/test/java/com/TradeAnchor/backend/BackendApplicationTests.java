58ebb80a7ac47cedf7bcb17f3053e4776244b024
rish990 <rishikabora142004@gmail.com>
2025-12-15 19:04:52 +0530

    remake backend folder

---
+++ backend /src/test/java/com/TradeAnchor/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TradeAnchor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

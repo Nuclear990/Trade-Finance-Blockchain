ef7cd6b1a845611ad021db261c73fe207798ddc0
rish990 <rishikabora142004@gmail.com>
2025-12-18 18:33:07 +0530

    redo backend folder

--- backend/src/main/java/com/tradeAnchor/backend/Backend.java
+++ backend/src/main/java/com/tradeAnchor/backend/Backend.java
@@ -1,13 +1,13 @@
-package com.TradeAnchor.backend;
+package com.tradeAnchor.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class Backend {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(Backend.class, args);
 	}
 
 }

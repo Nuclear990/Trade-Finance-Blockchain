86081dfbbda7f5eaebdcc66007329764232b8e82
rish990 <rishikabora142004@gmail.com>
2025-12-18 18:33:07 +0530

    redo backend folder

--- backend/src/test/java/com/tradeAnchor/backend/DemoApplicationTests.java
+++ backend/src/test/java/com/tradeAnchor/backend/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.TradeAnchor.backend;
+package com.tradeAnchor.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
